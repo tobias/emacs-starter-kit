6ac9b61f567163f558a79819da12d57dd0981cbb
Tobias Crawley <tcrawley@gmail.com>
2010-02-13 10:17:58 -0500

    Let's keep track of custom.el, shall we?

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(confirm-kill-emacs (quote y-or-n-p))
+ '(cua-mode t nil (cua-base))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(transient-mark-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(linum ((t (:inherit (shadow default) :background "grey15" :foreground "grey40")))))
