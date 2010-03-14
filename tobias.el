a51a8cb398e238ce6ebe2478c042cb91f65c9f8e
Tobias Crawley <tcrawley@gmail.com>
2010-03-14 11:31:30 -0400

    Added swap-windows function.

--- tobias.el
+++ tobias.el
@@ -144,4 +144,26 @@
 ;; disable fringe
 (set-fringe-mode 0)
 
+(global-auto-revert-mode t)
+
+;; from http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((not (= (count-windows) 2))
+         (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1)))))
+
+(global-set-key [ (control c) (control s) ] 'swap-windows)
+
 (require 'comment-ruby)
