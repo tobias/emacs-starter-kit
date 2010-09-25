0974bc96e9e9d1fd594e9bf2c206a86fbe96d21c
Tobias Crawley <tcrawley@gmail.com>
2010-09-25 14:47:27 -0400

    Fix mac growl arguments.

--- vendor/todochiku.el
+++ vendor/todochiku.el
@@ -263,11 +263,9 @@ See the variable `todochiku-icons' for a list of available icons."
 This would be better done through a customization probably."
   (case system-type
     ('windows-nt (list "/M" title message icon))
-    ('darwin (list title (and sticky "-s") "-m" message "--image" icon ))
+    ('darwin (list title (if sticky "-s" "") "-m" message "--image" icon ))
     (t (list "-i" icon "-t"
-             (if sticky
-                 "0"
-               (int-to-string (* 1000 todochiku-timeout)))
+             (if sticky "0" (int-to-string (* 1000 todochiku-timeout)))
              title message))))
 
 (defun todochiku-icon (icon)
